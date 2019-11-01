3164ebc63dfb95072b3051050e3deb28b9a09102
mariush2 <marius@mh2.no>
2019-11-01 16:14:20 +0100

    style: Added local font

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,27 @@ export default {
   name: 'App',
 };
 </script>
+
+<style lang="scss">
+@font-face {
+  font-family: 'Comfortaa';
+  src: url('~@/assets/fonts/Comfortaa-Regular.ttf') format('truetype');
+  font-weight: 400;
+}
+
+@font-face {
+  font-family: 'Comfortaa';
+  src: url('~@/assets/fonts/Comfortaa-Light.ttf') format('truetype');
+  font-weight: 200;
+}
+
+@font-face {
+  font-family: 'Comfortaa';
+  src: url('~@/assets/fonts/Comfortaa-Bold.ttf') format('truetype');
+  font-weight: 600;
+}
+
+#app {
+  font-family: 'Comfortaa', sans-serif;
+}
+</style>
