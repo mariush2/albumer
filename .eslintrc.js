c34a49f67d0e17c8d8499f243379e6eff6acb9f2
mariush2 <marius@mh2.no>
2019-11-01 12:40:40 +0100

    fix: Fixed eslint/prettier configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,14 @@ module.exports = {
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "prettier/prettier": [
+      "warn",
+      {
+	"singleQuote": true,
+	"trailingComma": "es5"
+      }
+    ]
   },
   parserOptions: {
     parser: "babel-eslint"
