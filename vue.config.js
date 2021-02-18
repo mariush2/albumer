81327a62543a3f7cfabb7955e23806d83474c0d9
mariush2 <marius@mh2.no>
2021-02-18 14:18:28 +0100

    Fixed notch on iphone

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
-/*module.exports = {
-  transpileDependencies: ['vuetify'],
-};*/
+module.exports = {
+  pwa: {
+    name: 'albumer',
+    themeColor: '#b65b6b',
+    msTileColor: '#b65b6b',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black-translucent',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+  },
+};
